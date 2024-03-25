1282a5da64b0085bcbd55c9b8566d20635b9b647
Thaniyakit <thaniyakit@gmail.com>
2024-03-26 00:01:29 +0700

    edit next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images:{
-		domains: ['drive.google.com'],
+		remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+                port: '',
+                pathname: '**',
+            },
+        ],
 	},
 	experimental: {
 		serverActions: true
