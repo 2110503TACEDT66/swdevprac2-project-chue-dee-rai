befc67b96fddade1eea6993a857b814b8b1943e7
Thaniyakit <thaniyakit@gmail.com>
2024-03-25 20:32:53 +0700

    Update Banner

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -28,7 +28,7 @@ export default async function TopMenu(){
 			
 			
 			<div className='flex flex-row absolute right-0 h-full'>
-				<TopMenuItem title= 'Cart' pageRef='/cart'/>
+				<TopMenuItem title= 'Booking' pageRef='/cart'/>
 				{
 					session? <Link href = "/api/auth/signout">
 						<div className='flex items-center h-full px-2 text-cyan-600 text-sm'>
